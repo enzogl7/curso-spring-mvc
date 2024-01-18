3a6cf163f21e997becae57eff2ff0e397c073e79
Enzo Lima <enzolima527@gmail.com>
2024-01-18 15:19:21 -0300

    .

--- cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -2,7 +2,9 @@ package com.algaworks.cobranca.model;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.Objects;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -22,19 +24,64 @@ public class Titulo {
 	
 	private String descricao;
 	
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
-	
+	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
+	
 	@Enumerated(EnumType.STRING)
 	private StatusTitulo status;
-	
-	
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public StatusTitulo getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusTitulo status) {
+		this.status = status;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(codigo);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -44,40 +91,12 @@ public class Titulo {
 		if (getClass() != obj.getClass())
 			return false;
 		Titulo other = (Titulo) obj;
-		return Objects.equals(codigo, other.codigo);
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
 	}
-	
-	public Long getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public Date getDataVencimento() {
-		return dataVencimento;
-	}
-	public void setDataVencimento(Date dataVencimento) {
-		this.dataVencimento = dataVencimento;
-	}
-	public BigDecimal getValor() {
-		return valor;
-	}
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-	public StatusTitulo getStatus() {
-		return status;
-	}
-	public void setStatus(StatusTitulo status) {
-		this.status = status;
-	}
-	
-	
-	
+
 }
