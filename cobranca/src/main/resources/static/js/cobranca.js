5d77795941ed4ee731906ce2b50b2856785e2514
Enzo Lima <enzolima527@gmail.com>
2024-01-19 11:18:24 -0300

    .

--- cobranca/src/main/resources/static/js/cobranca.js
+++ cobranca/src/main/resources/static/js/cobranca.js
@@ -18,4 +18,6 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 
 $(function() {
 	$('[rel="tooltip"]').tooltip();
-})
+	$('.js-currency').maskMoney({decimal: ',', thousands: '.', allowZero: true});
+	
+});
